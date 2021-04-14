73a72d896a809a294b89f1fa6472b66e366076c9
sid10on10 <siddhantk.singh.phe14@itbhu.ac.in>
2021-04-14 22:37:01 +0530

    enquiry

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var mongodb = require('mongodb')
 var mongodClient = mongodb.MongoClient;
-//var url = "mongodb+srv://sid10on10:qwerty123@cluster0.fqer8.mongodb.net/shortener?retryWrites=true&w=majority";
-var url = "mongodb://sid:sid@127.0.0.1:27017"
+var url = "mongodb+srv://sid10on10:achin@acquainfra@cluster0.mwrzo.mongodb.net/acquainfra?retryWrites=true&w=majority";
+//var url = "mongodb://sid:sid@127.0.0.1:27017"
 
 module.exports = {url,mongodClient}
